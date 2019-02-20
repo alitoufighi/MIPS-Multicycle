3e7c307b115db535b7913c697a7c8a9cedd0dec1
Mohammad Ali Toufighi <alitoumob@gmail.com>
2019-02-20 12:01:23 +0000

    modify seven segment

--- seven-segment.v
+++ seven-segment.v
@@ -1,22 +1,22 @@
 module seven_segment(input[3:0] in, output reg [6:0] out);
 	always @(*) begin
 		case(in)
-			4'd0: out <= 7'b1011111;
+			4'd0: out <= 7'b1111110;
 			4'd1: out <= 7'b0000011;
-			4'd2: out <= 7'b1011011;
+			4'd2: out <= 7'b1101101;
 			4'd3: out <= 7'b1011111;
 			4'd4: out <= 7'b1100110;
-			4'd5: out <= 7'b1101101;
-			4'd6: out <= 7'b1111101;
-			4'd7: out <= 7'b0000111;
+			4'd5: out <= 7'b1011011;
+			4'd6: out <= 7'b1011111;
+			4'd7: out <= 7'b1110000;
 			4'd8: out <= 7'b1111111;
-			4'd9: out <= 7'b1101111;
+			4'd9: out <= 7'b1111011;
 			4'ha: out <= 7'b1110111;
-			4'hb: out <= 7'b1111100;
-			4'hc: out <= 7'b0111001;
-			4'hd: out <= 7'b1011110;
-			4'he: out <= 7'b1111001;
-			4'hf: out <= 7'b1110001;
+			4'hb: out <= 7'b0011111;
+			4'hc: out <= 7'b1001110;
+			4'hd: out <= 7'b0111101;
+			4'he: out <= 7'b1001111;
+			4'hf: out <= 7'b1000111;
 		endcase
 	end
 endmodule
