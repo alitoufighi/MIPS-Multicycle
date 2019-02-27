104616ea6ceb176fecad4ca75332c0e8b8c698a3
Mohammad Ali Toufighi <alitoumob@gmail.com>
2019-02-27 15:14:09 +0330

    update prototype

--- mem-stage-reg.v
+++ mem-stage-reg.v
@@ -1,8 +1,22 @@
-module MEM_Stage_reg(input clk, rst, input[31:0] PC_in, output reg[31:0] PC);
-	always @(posedge clk, posedge rst) begin
-		if(rst)
-			PC <= 32'b0;
-		else
-			PC <= PC_in;
-	end
+module MEM_Stage_reg(
+	input clk,
+	input rst,
+	input WB_en_in,
+	input MEM_R_EN_in,
+	input [31:0] ALU_result_in,
+	input [31:0] Mem_read_value_in,
+	input [4:0] Dest_in,
+	input [31:0] PC_in,
+	output reg WB_en;
+	output reg MEM_R_EN,
+	output reg [31:0] ALU_result,
+	output reg [31:0] MeM_read_value,
+	output reg [4:0] Dest,
+	output reg[31:0] PC);
+// 	always @(posedge clk, posedge rst) begin
+// 		if(rst)
+// 			PC <= 32'b0;
+// 		else
+// 			PC <= PC_in;
+// 	end
 endmodule
