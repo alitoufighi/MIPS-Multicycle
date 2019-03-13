a8d186f8a08e4eba462b7cd30710d525c0b8caef
Mohammad Ali Toufighi <alitoumob@gmail.com>
2019-03-13 12:52:32 +0330

    complete MEM_STAGE -- not tested

--- mem-stage.v
+++ mem-stage.v
@@ -6,5 +6,19 @@ module MEM_Stage(
 	input [31:0] ST_val,
 	output [31:0] Mem_read_value
 	);
-	// assign PC = PC_in;
+	wire [31:0] mem_addr;
+
+	Address_Mapping address_mapping(
+		.addr_in(ALU_result_in),
+		.addr_out(mem_addr)
+	);
+
+	Data_Memory data_memory(
+		.clk(clk),
+		.addr(mem_addr),
+		.write_val(ST_val),
+		.MEM_R_EN(MEM_R_EN_in),
+		.MEM_W_EN(MEM_W_EN_in),
+		.read_val(Mem_read_value)
+	);
 endmodule
