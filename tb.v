10c5393d817768b6c9b1b242cce5a8115064db22
Mohammad Ali Toufighi <alitoumob@gmail.com>
2019-03-28 18:52:28 +0430

    add testbench

--- tb.v
+++ tb.v
@@ -1,7 +1,11 @@
+`timescale 1ns/1ns
 module tb();
-    MIPS(.clk(clk), .rst(rst));
-
-    wire clk = 0, rst = 1;
-    
-
+  reg clk = 0, rst = 1;
+  wire [17:0]s;
+  assign s = {14'b0, rst, 3'b0};
+  
+    MIPS mips(.CLOCK_50(clk), .SW(s));
+	
+    initial repeat(1000)#10 clk=~clk;
+	initial repeat(1)#25 rst=~rst;
 endmodule
